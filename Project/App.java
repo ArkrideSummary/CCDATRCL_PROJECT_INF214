f71999bad50e3d708ed2de187ef3e534558f8067
Tanghal, J-rhyzz <111556779+ArkrideSummary@users.noreply.github.com>
2022-11-08 05:24:12 +0800

    Create App.java

---
+++ Project/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        SimpleHTLib yeet = new SimpleHTLib();
+        System.out.println("Welcome to National University's Library.");
+        yeet.choices();
+        }
+}
